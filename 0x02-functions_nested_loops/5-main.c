a98e6eaba4927131bc568cebeb3fcadf46fd9621
“teyvi” <teyviangela@yahoo.comgit config --global user.email teyviangela@yahoo.com>
2023-08-17 16:14:25 -0700

    h

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -22,12 +22,4 @@ int main(void)
     r = print_sign(0xff);
     _putchar(',');
     _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
+    _putchar}
