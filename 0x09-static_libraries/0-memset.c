7dfe191b898ecc126b10b9fd6dc07df2288a6360
teyvi <teyviangela@yahoo.com>
2023-09-10 08:56:25 -0700

    fill a block of memeory with specific value

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset -fill a block of memory with a specific value
+ * Retur: changed array with new value
+ */
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
