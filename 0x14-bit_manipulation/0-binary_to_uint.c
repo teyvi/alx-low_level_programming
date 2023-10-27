7a557397d5c6646dd31039c31dde3026c09e8441
teyvi <teyviangela@yahoo.com>
2023-10-27 00:43:55 -0700

    init readme, main.h and 0-binary_to_unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Binary_to_unit -Converts a binary number to an unsigned int
+ * @b: A pointer to a string of 0 and 1 characters.
+ *
+ * Returns: The converted number, or 0 if there's an error.
+ */
+
+unsigned int binary_to_unit(const char *b)
+
+{
+	unsigned int result = 0;
+
+
+}
